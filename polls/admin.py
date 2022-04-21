c7b81f615835644ea8b00d72bbf57e4dd14b7ffc
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-21 15:19:43 +0300

    initiall commit

--- polls/admin.py
+++ polls/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.site_header = "Pollster Admin"
-admin.site.site_title = "Pollster Admin Area"
-admin.site.index_title = "Welcome to the Pollster admin area"
+admin.site.site_header = "Capstone Admin"
+admin.site.site_title = "Capstone Admin Area"
+admin.site.index_title = "Welcome to the Capstone admin area"
 
 
 class ChoiceInline(admin.TabularInline):
@@ -16,8 +16,5 @@ class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [(None, {'fields': ['question_text']}),
                  ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
     inlines = [ChoiceInline]
-
-
-# admin.site.register(Question)
-# admin.site.register(Choice)
+    
 admin.site.register(Question, QuestionAdmin)
