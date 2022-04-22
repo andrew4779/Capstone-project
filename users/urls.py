8e037701c23d4d62068d6c387149e67b732a0103
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-22 10:18:00 +0300

    deployment to heroku

--- users/urls.py
+++ users/urls.py
@@ -10,3 +10,4 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/Userlogout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
 ]
+
