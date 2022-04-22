9d8e71747cedb6c74e55b879e66613e225592802
andrew4779 <andrew.kimani@student.moringaschool.com>
2022-04-22 10:26:17 +0300

    deployment to heroku

--- capstone/settings.py
+++ capstone/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +141,4 @@ messages.ERROR: 'alert-danger',
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
